2c46faf75d53ff77e6a119df242f527069fd31a1
Carsten Wache <carsten.wache@googlemail.com>
2019-07-23 12:32:10 +0200

    increase to 118 for GPS. Add condition to kissSerial

--- js/plugins/jquery.kiss.serial.js
+++ js/plugins/jquery.kiss.serial.js
@@ -18,7 +18,9 @@
             c += '<option value="3" data-i18n="serialtype.3">VTX</option>';
             c += '<option value="4" data-i18n="serialtype.4">ESC TLM</option>';
             c += '<option value="5" data-i18n="serialtype.5">Runcam</option>';
-            c += '<option value="6" data-i18n="serialtype.5">VTX + ESC TLM</option>';
+            c += '<option value="6" data-i18n="serialtype.6">VTX + ESC TLM</option>';
+            if (data.version > 117)
+                c += '<option value="7" data-i18n="serialtype.7">GPS</option>';
             c += '</select></dd>';
             self.empty();
             self.append(c);
